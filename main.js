8295a8d6bf92101a2933ef00559e2c4aca4fab2d
Francesco Bonanno <francescosoapbonanno@gmail.com>
2023-11-12 10:48:21 +0100

    base creata, console log richiama gli array mostrandone il contenuto

---
+++ main.js
@@ -0,0 +1,7 @@
+ const persone ={
+    nome :["wayne barrett" , "angela Caroll" , "Gordon Freeman" , "Angela Lopez" , "Scott Estrada" , "Barbara Ramos"] ,
+
+    professione : ["Founder & CEO", "Chief Editor" , "Office Manager" , "Social Media Manager" , "Developer" , "Graphic Designer"]
+
+};
+console.log(persone.professione);

7948440b571920df04ed61e2d37d23945f75c358
Francesco Bonanno <francescosoapbonanno@gmail.com>
2023-11-12 11:24:47 +0100

    ciclo for che mostra su schermo l'intero array

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
- const persone ={
+ const persone = {
     nome :["wayne barrett" , "angela Caroll" , "Gordon Freeman" , "Angela Lopez" , "Scott Estrada" , "Barbara Ramos"] ,
 
     professione : ["Founder & CEO", "Chief Editor" , "Office Manager" , "Social Media Manager" , "Developer" , "Graphic Designer"]
 
 };
-console.log(persone.professione);
+for (let nome in persone){
+    console.log(persone[nome]);
+    let identity = document.getElementById("identita").innerText =(persone[nome]);
+}
